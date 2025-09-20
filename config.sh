28922c6e6e4f799b5bd6684101985d32d47751e5
micropuma <micropuma@163.com>
2025-09-20 16:44:12 +0800

    [script] script for building triton from source

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
 source .venv/bin/activate
 
 export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libstdc++.so.6
-export PATH=/home/douliyang/large/triton-workspace/triton-tutorial/build/cmake.linux-x86_64-cpython-3.11/bin:$PATH
-export PYTHONPATH=/mnt/home/douliyang/triton-workspace/triton-tutorial/python:$PYTHONPATH
+export PATH=/home/douliyang/large/triton-workspace/triton-dly/build/cmake.linux-x86_64-cpython-3.12/bin:$PATH
+export PYTHONPATH=/home/douliyang/large/triton-workspace/triton-dly/python:$PYTHONPATH
 
--- install.sh
+++ install.sh
@@ -2,9 +2,14 @@
 pip uninstall triton
 
 export DEBUG=1
-export LLVM_DIR=/home/douliyang/large/triton-workspace/llvm-project/build
+export LLVM_BUILD_DIR=/home/douliyang/large/triton-workspace/llvm-project/build
+export PATH=/home/douliyang/large/triton-workspace/llvm-project/build/bin:$PATH
 
-LLVM_SYSPATH=$LLVM_DIR \
-LLVM_INCLUDE_DIRS=$LLVM_DIR/include \
-LLVM_LIBRARY_DIR=$LLVM_DIR/lib \
+export LLVM_DIR=$LLVM_BUILD_DIR/lib/cmake/llvm
+export MLIR_DIR=$LLVM_BUILD_DIR/lib/cmake/mlir
+
+LLVM_INCLUDE_DIRS=$LLVM_BUILD_DIR/include \
+MLIR_DIR=$LLVM_INCLUDE_DIRS \
+LLVM_LIBRARY_DIR=$LLVM_BUILD_DIR/lib \
+LLVM_SYSPATH=$LLVM_BUILD_DIR \
 pip install -e . -i https://mirrors.aliyun.com/pypi/simple/
